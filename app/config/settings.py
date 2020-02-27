16e3eb25d6a8b4b9e7319b992bfad056ad731161
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-27 15:00:31 +0900

    changed to db sqlit

--- app/config/settings.py
+++ app/config/settings.py
@@ -107,7 +107,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 """postgresql 사용"""
-DATABASES = SECRETS['DATABASES']
+# DATABASES = SECRETS['DATABASES']  AWS RDS Postgresql 데이터 베이스
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
