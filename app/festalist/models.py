9ef35af4314796060a2869c1baa03b4c299d1e6b
eqfwcev123 <dohyeonee95@hotmail.com>
2020-03-03 20:01:32 +0900

    .

--- app/festalist/models.py
+++ app/festalist/models.py
@@ -1,11 +1,12 @@
+# Create your models here.
 from django.db import models
 
 # Create your models here.
-from app.members.models import User
+from members.models import User
 
 
 class FestaList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150, blank=True)
     organizer = models.CharField(max_length=150, blank=True)
     date = models.CharField(max_length=150, blank=True)
