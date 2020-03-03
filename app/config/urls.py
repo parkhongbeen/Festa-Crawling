338954f8c21bda15d64f4db561e405c4b73efe34
eqfwcev123 <dohyeonee95@hotmail.com>
2020-03-03 20:05:51 +0900

    allauth settings

--- app/config/urls.py
+++ app/config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+urlpatterns_api =[
+    path('members/', include('members.urls'))
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(urlpatterns_api)),
+    path('accounts/', include('allauth.urls')),
 ]
