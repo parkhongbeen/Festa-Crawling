dd61904733ce60fe292c1c78c1717a7e0356ecf2
eqfwcev123 <dohyeonee95@hotmail.com>
2020-03-05 17:59:52 +0900

    changed serializer fields

--- app/festalist/serializers.py
+++ app/festalist/serializers.py
@@ -1,34 +1,9 @@
 from rest_framework import serializers
+
 from .models import FestaList
 
 
 class FestaListSerializer(serializers.ModelSerializer):
     class Meta:
         model = FestaList
-        fields = (
-            'pk',
-            'author',
-            'title',
-            'code',
-            'linenos',
-            'language',
-            'style',
-            'created',
-        )
-
-
-# class FestaListCreateSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = FestaList
-#         fields = (
-#             'pk',
-#             'author',
-#             'title',
-#             'code',
-#             'linenos',
-#             'language',
-#             'style',
-#         )
-#
-#         def to_representation(self, instance):
-#             return FestaListSerializer(instance).data
+        fields = ['title', 'host', 'date', 'content', 'apply', 'tickets', 'link', 'image']
